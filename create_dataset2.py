c4f3bbbf5df0b1f40f9cf1a9502847ff04ef2061
Dzikri Dhaniawan <dhaniawan.dzikri@gmail.com>
2024-12-15 23:14:52 +0800

    tamat

--- create_dataset2.py
+++ create_dataset2.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 
 import mediapipe as mp
 import cv2
@@ -13,8 +14,12 @@ hands = mp_hands.Hands(static_image_mode=True, max_num_hands=2,min_detection_con
 
 DATA_DIR= './data'
 
+
+data = []
+labels = []
 for dir_ in os.listdir(DATA_DIR):
-    for img_path in os.listdir(os.path.join(DATA_DIR, dir_))[:1]:
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
+        data_aux = []
         img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
@@ -22,9 +27,14 @@ for dir_ in os.listdir(DATA_DIR):
         if result.multi_hand_landmarks:
             for hand_landmarks in result.multi_hand_landmarks:
                 for i in range(len(hand_landmarks.landmark)):
-                    print(hand_landmarks.landmark[i])
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x)
+                    data_aux.append(y)
 
-        plt.figure()
-        plt.imshow(img_rgb)
+            data.append(data_aux)
+            labels.append(dir_)
 
-plt.show()
+f = open('data.pickle', 'wb')
+pickle.dump({'data': data, 'labels': labels}, f)
+f.close()
